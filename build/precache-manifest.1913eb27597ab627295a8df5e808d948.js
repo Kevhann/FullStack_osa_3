441b966ff252ff8d1da353c4a2ec681a655c8948
Kevhann <kevin.hann@helsinki.fi>
2019-01-28 15:24:52 +0200

    lokaali url

---
+++ build/precache-manifest.1913eb27597ab627295a8df5e808d948.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "f1e76c2a6f6eb2f05ab2",
+    "url": "/static/js/main.f1e76c2a.chunk.js"
+  },
+  {
+    "revision": "1bdeb50c48bfe976fdd4",
+    "url": "/static/js/1.1bdeb50c.chunk.js"
+  },
+  {
+    "revision": "532188220653f77a9601c3f3b088df14",
+    "url": "/index.html"
+  }
+];
